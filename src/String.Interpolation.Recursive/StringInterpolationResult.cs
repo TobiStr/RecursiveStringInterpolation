fa5f45c9db3188a6e384be79ca4558e533589b88
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2020-10-28 17:09:12 +0100

    :heavy_plus_sign: Add internal InterpolationResult class

---
+++ src/String.Interpolation.Recursive/StringInterpolationResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace String.Interpolation.Recursive
+{
+    internal class StringInterpolationResult
+    {
+        public StringInterpolationResult(string value, string originalKey) {
+            if (string.IsNullOrWhiteSpace(value)) {
+                throw new ArgumentException($"'{nameof(value)}' cannot be null or whitespace", nameof(value));
+            }
+
+            if (string.IsNullOrWhiteSpace(originalKey)) {
+                throw new ArgumentException($"'{nameof(originalKey)}' cannot be null or whitespace", nameof(originalKey));
+            }
+
+            Value = value;
+            OriginalKey = originalKey;
+        }
+
+        public string Value { get; }
+
+        public string OriginalKey { get; }
+    }
+}
