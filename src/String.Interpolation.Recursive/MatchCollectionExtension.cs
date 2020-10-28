067019f0f7bff25da4fd776526116085c74b86e4
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2020-10-28 17:08:20 +0100

    :heavy_plus_sign:  Add Regex MatchCollectionExtension

---
+++ src/String.Interpolation.Recursive/MatchCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace String.Interpolation.Recursive
+{
+    internal static class MatchCollectionExtension
+    {
+        public static IEnumerable<string> AsValueEnumerable(this MatchCollection matchCollection) {
+            var enumerator = matchCollection.GetEnumerator();
+            enumerator.Reset();
+            while (enumerator.MoveNext()) {
+                yield return ((Match)enumerator.Current).Groups[1].Value;
+            }
+        }
+    }
+}
