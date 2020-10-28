714d409253c9380343e978867d5e0a9c32f293c6
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2020-10-28 17:08:39 +0100

    :heavy_plus_sign: Add InterpolationException

---
+++ src/String.Interpolation.Recursive/InterpolationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace String.Interpolation.Recursive
+{
+    [Serializable]
+    public class InterpolationException : Exception
+    {
+        public InterpolationException() {
+        }
+
+        public InterpolationException(string message) : base(message) {
+        }
+
+        public InterpolationException(string message, Exception innerException) : base(message, innerException) {
+        }
+
+        protected InterpolationException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}
